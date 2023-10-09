5022b320b7f1e2c31862857d388efcc10100fc39
Colin Z <colin@2008-MacBook.local>
2023-10-08 22:39:50 -0400

    add cmz edits

--- arris-stats.sh
+++ arris-stats.sh
@@ -34,10 +34,10 @@
 ###############               Configuration            #####################
 # TEMP is the full path to the temp file wget will grab from your modem.
 #      it is recommended to use something in tmpfs like /tmp for example.
-TEMP=/tmp/snapshot.html
+TEMP=/Users/colin/Software/arris-capture/tmp/snapshot.html
 
 # LOGPATH is the full path to the log file you will keep.
-LOGPATH=/tmp/mnt/router/optware/share/www/arris
+LOGPATH=/Users/colin/Software/arris-capture/tmp/arris
 
 # If you want to log all html snapshots set KEEPTHEM to any value
 # Be sure that you have sufficient storage for this as files are around 5 Kb
@@ -59,57 +59,84 @@ RUNDATE=$(date "+%F %T")
 [[ -f "$TEMP" ]] && rm -f "$TEMP"
 
 # try to get stats 6 times waiting 10 sec per time
-wget -q -T 10 -t 6 http://192.168.100.1/cgi-bin/status_cgi -O $TEMP || fail
+wget -q -T 10 -t 6 http://192.168.100.1/RgConnect.asp -O $TEMP || fail
 
 if [[ -n "$KEEPTHEM" ]]; then
 	SAVE="$LOGPATH/snapshots"
-	CAPDATE=$(date -d "$RUNDATE" "+%Y%m%d_%H%M%S")
+	CAPDATE=$(date "+%Y%m%d_%H%M%S")
 	[[ ! -d "$SAVE" ]] && mkdir "$SAVE"
 	[[ -f "$TEMP" ]] && cp "$TEMP" "$SAVE/$CAPDATE.html"
 fi
 
 # find downstream frequencies
-DF1=$(grep 'Downstream 1' $TEMP | awk -F'<td>' '{ print $4 }' | sed 's/<.*//')
-DF2=$(grep 'Downstream 2' $TEMP | awk -F'<td>' '{ print $13 }' | sed 's/<.*//')
-DF3=$(grep 'Downstream 3' $TEMP | awk -F'<td>' '{ print $22 }' | sed 's/<.*//')
-DF4=$(grep 'Downstream 4' $TEMP | awk -F'<td>' '{ print $31 }' | sed 's/<.*//')
-DF5=$(grep 'Downstream 5' $TEMP | awk -F'<td>' '{ print $40 }' | sed 's/<.*//')
-DF6=$(grep 'Downstream 6' $TEMP | awk -F'<td>' '{ print $49 }' | sed 's/<.*//')
-DF7=$(grep 'Downstream 7' $TEMP | awk -F'<td>' '{ print $58 }' | sed 's/<.*//')
-DF8=$(grep 'Downstream 8' $TEMP | awk -F'<td>' '{ print $67 }' | sed 's/<.*//')
+DF1=$(grep -a '<tr><td>1</td><td>Locked</td><td>QAM256</td>' $TEMP | awk -F'<td>' '{ print $6 }' | sed 's| Hz.*||')
+DF2=$(grep -a '<tr><td>2</td><td>Locked</td><td>QAM256</td>' $TEMP | awk -F'<td>' '{ print $6 }' | sed 's| Hz.*||')
+DF3=$(grep -a '<tr><td>3</td><td>Locked</td><td>QAM256</td>' $TEMP | awk -F'<td>' '{ print $6 }' | sed 's| Hz.*||')
+DF4=$(grep -a '<tr><td>4</td><td>Locked</td><td>QAM256</td>' $TEMP | awk -F'<td>' '{ print $6 }' | sed 's| Hz.*||')
+DF5=$(grep -a '<tr><td>5</td><td>Locked</td><td>QAM256</td>' $TEMP | awk -F'<td>' '{ print $6 }' | sed 's| Hz.*||')
+DF6=$(grep -a '<tr><td>6</td><td>Locked</td><td>QAM256</td>' $TEMP | awk -F'<td>' '{ print $6 }' | sed 's| Hz.*||')
+DF7=$(grep -a '<tr><td>7</td><td>Locked</td><td>QAM256</td>' $TEMP | awk -F'<td>' '{ print $6 }' | sed 's| Hz.*||')
+DF8=$(grep -a '<tr><td>8</td><td>Locked</td><td>QAM256</td>' $TEMP | awk -F'<td>' '{ print $6 }' | sed 's| Hz.*||')
+DF9=$(grep -a '<tr><td>9</td><td>Locked</td><td>QAM256</td>' $TEMP | awk -F'<td>' '{ print $6 }' | sed 's| Hz.*||')
+DF10=$(grep -a '<tr><td>10</td><td>Locked</td><td>QAM256</td>' $TEMP | awk -F'<td>' '{ print $6 }' | sed 's| Hz.*||')
+DF11=$(grep -a '<tr><td>11</td><td>Locked</td><td>QAM256</td>' $TEMP | awk -F'<td>' '{ print $6 }' | sed 's| Hz.*||')
+DF12=$(grep -a '<tr><td>12</td><td>Locked</td><td>QAM256</td>' $TEMP | awk -F'<td>' '{ print $6 }' | sed 's| Hz.*||')
+DF13=$(grep -a '<tr><td>13</td><td>Locked</td><td>QAM256</td>' $TEMP | awk -F'<td>' '{ print $6 }' | sed 's| Hz.*||')
+DF14=$(grep -a '<tr><td>14</td><td>Locked</td><td>QAM256</td>' $TEMP | awk -F'<td>' '{ print $6 }' | sed 's| Hz.*||')
+DF15=$(grep -a '<tr><td>15</td><td>Locked</td><td>QAM256</td>' $TEMP | awk -F'<td>' '{ print $6 }' | sed 's| Hz.*||')
+DF16=$(grep -a '<tr><td>16</td><td>Locked</td><td>QAM256</td>' $TEMP | awk -F'<td>' '{ print $6 }' | sed 's| Hz.*||')
 
 # downstream power
-DP1=$(grep "$DF1" $TEMP | awk -F'<td>' '{ print $5 }' | sed 's| dBmV.*||')
-DP2=$(grep "$DF2" $TEMP | awk -F'<td>' '{ print $14 }' | sed 's| dBmV.*||')
-DP3=$(grep "$DF3" $TEMP | awk -F'<td>' '{ print $23 }' | sed 's| dBmV.*||')
-DP4=$(grep "$DF4" $TEMP | awk -F'<td>' '{ print $32 }' | sed 's| dBmV.*||')
-DP5=$(grep "$DF5" $TEMP | awk -F'<td>' '{ print $41 }' | sed 's| dBmV.*||')
-DP6=$(grep "$DF6" $TEMP | awk -F'<td>' '{ print $50 }' | sed 's| dBmV.*||')
-DP7=$(grep "$DF7" $TEMP | awk -F'<td>' '{ print $59 }' | sed 's| dBmV.*||')
-DP8=$(grep "$DF8" $TEMP | awk -F'<td>' '{ print $68 }' | sed 's| dBmV.*||')
+DP1=$(grep -a '<tr><td>1</td><td>Locked</td><td>QAM256</td>' $TEMP | awk -F'<td>' '{ print $7 }' | sed 's| dBmV.*||')
+DP2=$(grep -a '<tr><td>2</td><td>Locked</td><td>QAM256</td>' $TEMP | awk -F'<td>' '{ print $7 }' | sed 's| dBmV.*||')
+DP3=$(grep -a '<tr><td>3</td><td>Locked</td><td>QAM256</td>' $TEMP | awk -F'<td>' '{ print $7 }' | sed 's| dBmV.*||')
+DP4=$(grep -a '<tr><td>4</td><td>Locked</td><td>QAM256</td>' $TEMP | awk -F'<td>' '{ print $7 }' | sed 's| dBmV.*||')
+DP5=$(grep -a '<tr><td>5</td><td>Locked</td><td>QAM256</td>' $TEMP | awk -F'<td>' '{ print $7 }' | sed 's| dBmV.*||')
+DP6=$(grep -a '<tr><td>6</td><td>Locked</td><td>QAM256</td>' $TEMP | awk -F'<td>' '{ print $7 }' | sed 's| dBmV.*||')
+DP7=$(grep -a '<tr><td>7</td><td>Locked</td><td>QAM256</td>' $TEMP | awk -F'<td>' '{ print $7 }' | sed 's| dBmV.*||')
+DP8=$(grep -a '<tr><td>8</td><td>Locked</td><td>QAM256</td>' $TEMP | awk -F'<td>' '{ print $7 }' | sed 's| dBmV.*||')
+DP9=$(grep -a '<tr><td>9</td><td>Locked</td><td>QAM256</td>' $TEMP | awk -F'<td>' '{ print $7 }' | sed 's| dBmV.*||')
+DP10=$(grep -a '<tr><td>10</td><td>Locked</td><td>QAM256</td>' $TEMP | awk -F'<td>' '{ print $7 }' | sed 's| dBmV.*||')
+DP11=$(grep -a '<tr><td>11</td><td>Locked</td><td>QAM256</td>' $TEMP | awk -F'<td>' '{ print $7 }' | sed 's| dBmV.*||')
+DP12=$(grep -a '<tr><td>12</td><td>Locked</td><td>QAM256</td>' $TEMP | awk -F'<td>' '{ print $7 }' | sed 's| dBmV.*||')
+DP13=$(grep -a '<tr><td>13</td><td>Locked</td><td>QAM256</td>' $TEMP | awk -F'<td>' '{ print $7 }' | sed 's| dBmV.*||')
+DP14=$(grep -a '<tr><td>14</td><td>Locked</td><td>QAM256</td>' $TEMP | awk -F'<td>' '{ print $7 }' | sed 's| dBmV.*||')
+DP15=$(grep -a '<tr><td>15</td><td>Locked</td><td>QAM256</td>' $TEMP | awk -F'<td>' '{ print $7 }' | sed 's| dBmV.*||')
+DP16=$(grep -a '<tr><td>16</td><td>Locked</td><td>QAM256</td>' $TEMP | awk -F'<td>' '{ print $7 }' | sed 's| dBmV.*||')
 
 # downstream snr
-DS1=$(grep "$DF1" $TEMP | awk -F'<td>' '{ print $6 }' | sed 's| dB.*||')
-DS2=$(grep "$DF2" $TEMP | awk -F'<td>' '{ print $15 }' | sed 's| dB.*||')
-DS3=$(grep "$DF3" $TEMP | awk -F'<td>' '{ print $24 }' | sed 's| dB.*||')
-DS4=$(grep "$DF4" $TEMP | awk -F'<td>' '{ print $33 }' | sed 's| dB.*||')
-DS5=$(grep "$DF5" $TEMP | awk -F'<td>' '{ print $42 }' | sed 's| dB.*||')
-DS6=$(grep "$DF6" $TEMP | awk -F'<td>' '{ print $51 }' | sed 's| dB.*||')
-DS7=$(grep "$DF7" $TEMP | awk -F'<td>' '{ print $60 }' | sed 's| dB.*||')
-DS8=$(grep "$DF8" $TEMP | awk -F'<td>' '{ print $69 }' | sed 's| dB.*||')
+DS1=$(grep -a '<tr><td>1</td><td>Locked</td><td>QAM256</td>' $TEMP | awk -F'<td>' '{ print $8 }' | sed 's| dB.*||')
+DS2=$(grep -a '<tr><td>2</td><td>Locked</td><td>QAM256</td>' $TEMP | awk -F'<td>' '{ print $8 }' | sed 's| dB.*||')
+DS3=$(grep -a '<tr><td>3</td><td>Locked</td><td>QAM256</td>' $TEMP | awk -F'<td>' '{ print $8 }' | sed 's| dB.*||')
+DS4=$(grep -a '<tr><td>4</td><td>Locked</td><td>QAM256</td>' $TEMP | awk -F'<td>' '{ print $8 }' | sed 's| dB.*||')
+DS5=$(grep -a '<tr><td>5</td><td>Locked</td><td>QAM256</td>' $TEMP | awk -F'<td>' '{ print $8 }' | sed 's| dB.*||')
+DS6=$(grep -a '<tr><td>6</td><td>Locked</td><td>QAM256</td>' $TEMP | awk -F'<td>' '{ print $8 }' | sed 's| dB.*||')
+DS7=$(grep -a '<tr><td>7</td><td>Locked</td><td>QAM256</td>' $TEMP | awk -F'<td>' '{ print $8 }' | sed 's| dB.*||')
+DS8=$(grep -a '<tr><td>8</td><td>Locked</td><td>QAM256</td>' $TEMP | awk -F'<td>' '{ print $8 }' | sed 's| dB.*||')
+DS9=$(grep -a '<tr><td>9</td><td>Locked</td><td>QAM256</td>' $TEMP | awk -F'<td>' '{ print $8 }' | sed 's| dB.*||')
+DS10=$(grep -a '<tr><td>10</td><td>Locked</td><td>QAM256</td>' $TEMP | awk -F'<td>' '{ print $8 }' | sed 's| dB.*||')
+DS11=$(grep -a '<tr><td>11</td><td>Locked</td><td>QAM256</td>' $TEMP | awk -F'<td>' '{ print $8 }' | sed 's| dB.*||')
+DS12=$(grep -a '<tr><td>12</td><td>Locked</td><td>QAM256</td>' $TEMP | awk -F'<td>' '{ print $8 }' | sed 's| dB.*||')
+DS13=$(grep -a '<tr><td>13</td><td>Locked</td><td>QAM256</td>' $TEMP | awk -F'<td>' '{ print $8 }' | sed 's| dB.*||')
+DS14=$(grep -a '<tr><td>14</td><td>Locked</td><td>QAM256</td>' $TEMP | awk -F'<td>' '{ print $8 }' | sed 's| dB.*||')
+DS15=$(grep -a '<tr><td>15</td><td>Locked</td><td>QAM256</td>' $TEMP | awk -F'<td>' '{ print $8 }' | sed 's| dB.*||')
+DS16=$(grep -a '<tr><td>16</td><td>Locked</td><td>QAM256</td>' $TEMP | awk -F'<td>' '{ print $8 }' | sed 's| dB.*||')
+
 
 # find upstream frequencies
 # note my modem does not show a value for Upstream 2
 # so use a different strategy
 
-UF1=$(grep '<td>Upstream' $TEMP | awk -F'<td>' '{ print $4 }' | sed 's/<.*//')
-UF2=$(grep '<td>Upstream' $TEMP | awk -F'<td>' '{ print $11 }' | sed 's/<.*//')
-UF3=$(grep '<td>Upstream' $TEMP | awk -F'<td>' '{ print $18 }' | sed 's/<.*//')
+UF1=$(grep -a '<tr><td>1</td><td>Locked</td><td>ATDMA</td>' $TEMP | awk -F'<td>' '{ print $7 }' | sed 's| Hz.*||')
+UF2=$(grep -a '<tr><td>2</td><td>Locked</td><td>ATDMA</td>' $TEMP | awk -F'<td>' '{ print $7 }' | sed 's| Hz.*||')
+UF3=$(grep -a '<tr><td>3</td><td>Locked</td><td>ATDMA</td>' $TEMP | awk -F'<td>' '{ print $7 }' | sed 's| Hz.*||')
+UF4=$(grep -a '<tr><td>4</td><td>Locked</td><td>ATDMA</td>' $TEMP | awk -F'<td>' '{ print $7 }' | sed 's| Hz.*||')
 
 # upstream power
-UP1=$(grep "$UF1" $TEMP | awk -F'<td>' '{ print $5 }' | sed 's| dBmV.*||')
-UP2=$(grep "$UF2" $TEMP | awk -F'<td>' '{ print $12 }' | sed 's| dBmV.*||')
-UP3=$(grep "$UF3" $TEMP | awk -F'<td>' '{ print $19 }' | sed 's| dBmV.*||')
+UP1=$(grep -a '<tr><td>1</td><td>Locked</td><td>ATDMA</td>' $TEMP | awk -F'<td>' '{ print $8 }' | sed 's| dBmV.*||')
+UP2=$(grep -a '<tr><td>2</td><td>Locked</td><td>ATDMA</td>' $TEMP | awk -F'<td>' '{ print $8 }' | sed 's| dBmV.*||')
+UP3=$(grep -a '<tr><td>3</td><td>Locked</td><td>ATDMA</td>' $TEMP | awk -F'<td>' '{ print $8 }' | sed 's| dBmV.*||')
+UP4=$(grep -a '<tr><td>4</td><td>Locked</td><td>ATDMA</td>' $TEMP | awk -F'<td>' '{ print $8 }' | sed 's| dBmV.*||')
 
 # force a 0 value when undefined due to poor connectivity
 [[ "$DF1" = "----</td>" ]] && DF1=0
@@ -120,6 +147,15 @@ UP3=$(grep "$UF3" $TEMP | awk -F'<td>' '{ print $19 }' | sed 's| dBmV.*||')
 [[ "$DF6" = "----</td>" ]] && DF6=0
 [[ "$DF7" = "----</td>" ]] && DF7=0
 [[ "$DF8" = "----</td>" ]] && DF8=0
+[[ "$DF9" = "----</td>" ]] && DF9=0
+[[ "$DF10" = "----</td>" ]] && DF10=0
+[[ "$DF11" = "----</td>" ]] && DF11=0
+[[ "$DF12" = "----</td>" ]] && DF12=0
+[[ "$DF13" = "----</td>" ]] && DF13=0
+[[ "$DF14" = "----</td>" ]] && DF14=0
+[[ "$DF15" = "----</td>" ]] && DF15=0
+[[ "$DF16" = "----</td>" ]] && DF16=0
+
 [[ "$DP1" = "----</td>" ]] && DP1=0
 [[ "$DP2" = "----</td>" ]] && DP2=0
 [[ "$DP3" = "----</td>" ]] && DP3=0
@@ -128,6 +164,14 @@ UP3=$(grep "$UF3" $TEMP | awk -F'<td>' '{ print $19 }' | sed 's| dBmV.*||')
 [[ "$DP6" = "----</td>" ]] && DP6=0
 [[ "$DP7" = "----</td>" ]] && DP7=0
 [[ "$DP8" = "----</td>" ]] && DP8=0
+[[ "$DP9" = "----</td>" ]] && DP9=0
+[[ "$DP10" = "----</td>" ]] && DP10=0
+[[ "$DP11" = "----</td>" ]] && DP11=0
+[[ "$DP12" = "----</td>" ]] && DP12=0
+[[ "$DP13" = "----</td>" ]] && DP13=0
+[[ "$DP14" = "----</td>" ]] && DP14=0
+[[ "$DP15" = "----</td>" ]] && DP15=0
+[[ "$DP16" = "----</td>" ]] && DP16=0
 
 [[ "$DS1" = "----</td>" ]] && DS1=0
 [[ "$DS2" = "----</td>" ]] && DS2=0
@@ -137,20 +181,32 @@ UP3=$(grep "$UF3" $TEMP | awk -F'<td>' '{ print $19 }' | sed 's| dBmV.*||')
 [[ "$DS6" = "----</td>" ]] && DS6=0
 [[ "$DS7" = "----</td>" ]] && DS7=0
 [[ "$DS8" = "----</td>" ]] && DS8=0
+[[ "$DS9" = "----</td>" ]] && DS9=0
+[[ "$DS10" = "----</td>" ]] && DS10=0
+[[ "$DS11" = "----</td>" ]] && DS11=0
+[[ "$DS12" = "----</td>" ]] && DS12=0
+[[ "$DS13" = "----</td>" ]] && DS13=0
+[[ "$DS14" = "----</td>" ]] && DS14=0
+[[ "$DS15" = "----</td>" ]] && DS15=0
+[[ "$DS16" = "----</td>" ]] && DS16=0
 
 [[ "$UF1" = "----</td>" ]] && UF1=0
 [[ "$UF2" = "----</td>" ]] && UF2=0
 [[ "$UF3" = "----</td>" ]] && UF3=0
+[[ "$UF4" = "----</td>" ]] && UF4=0
 [[ -z "$UF1" ]] && UF1=0
 [[ -z "$UF2" ]] && UF2=0
 [[ -z "$UF3" ]] && UF3=0
+[[ -z "$UF4" ]] && UF4=0
 
 [[ "$UP1" = "----</td>" ]] && UP1=0
 [[ "$UP2" = "----</td>" ]] && UP2=0
 [[ "$UP3" = "----</td>" ]] && UP3=0
+[[ "$UP4" = "----</td>" ]] && UP4=0
 [[ -z "$UP1" ]] && UP1=0
 [[ -z "$UP2" ]] && UP2=0
 [[ -z "$UP3" ]] && UP3=0
+[[ -z "$UP4" ]] && UP4=0
 
 # The individual log files
 DLOGFREQ="$LOGPATH/downstream-freq.csv"
@@ -160,21 +216,21 @@ ULOGFREQ="$LOGPATH/upstream-freq.csv"
 ULOGPOWER="$LOGPATH/upstream-power.csv"
 
 # downstream frequency log
-[[ ! -f $DLOGFREQ ]] && echo "DTS,Downstream 1,Downstream 2,Downstream 3,Downstream 4,Downstream 5,Downstream 6,Downstream 7,Downstream 8" > $DLOGFREQ
-echo "$RUNDATE,${DF1/ *},${DF2/ *},${DF3/ *},${DF4/ *},${DF5/ *},${DF6/ *},${DF7/ *},${DF8/ *}" >> $DLOGFREQ
+[[ ! -f $DLOGFREQ ]] && echo "DTS,Downstream 1,Downstream 2,Downstream 3,Downstream 4,Downstream 5,Downstream 6,Downstream 7,Downstream 8,Downstream 9,Downstream 10,Downstream 11,Downstream 12,Downstream 13,Downstream 14,Downstream 15,Downstream 16" > $DLOGFREQ
+echo "$RUNDATE,${DF1/ *},${DF2/ *},${DF3/ *},${DF4/ *},${DF5/ *},${DF6/ *},${DF7/ *},${DF8/ *},${DF9/ *},${DF10/ *},${DF11/ *},${DF12/ *},${DF13/ *},${DF14/ *},${DF15/ *},${DF16/ *}" >> $DLOGFREQ
 
 # downstream power log
-[[ ! -f $DLOGPOWER ]] && echo "DTS,Downstream 1,Downstream 2,Downstream 3,Downstream 4,Downstream 5,Downstream 6,Downstream 7,Downstream 8" > $DLOGPOWER
-echo "$RUNDATE,$DP1,$DP2,$DP3,$DP4,$DP5,$DP6,$DP7,$DP8" >> $DLOGPOWER
+[[ ! -f $DLOGPOWER ]] && echo "DTS,Downstream 1,Downstream 2,Downstream 3,Downstream 4,Downstream 5,Downstream 6,Downstream 7,Downstream 8,Downstream 9,Downstream 10,Downstream 11,Downstream 12,Downstream 13,Downstream 14,Downstream 15,Downstream 16" > $DLOGPOWER
+echo "$RUNDATE,$DP1,$DP2,$DP3,$DP4,$DP5,$DP6,$DP7,$DP8,$DP9,$DP10,$DP11,$DP12,$DP13,$DP14,$DP15,$DP16" >> $DLOGPOWER
 
 # downstream SNR log
-[[ ! -f $DLOGSNR ]] && echo "DTS,Downstream 1,Downstream 2,Downstream 3,Downstream 4,Downstream 5,Downstream 6,Downstream 7,Downstream 8" > $DLOGSNR
-echo "$RUNDATE,$DS1,$DS2,$DS3,$DS4,$DS5,$DS6,$DS7,$DS8" >> $DLOGSNR
+[[ ! -f $DLOGSNR ]] && echo "DTS,Downstream 1,Downstream 2,Downstream 3,Downstream 4,Downstream 5,Downstream 6,Downstream 7,Downstream 8,Downstream 9,Downstream 10,Downstream 11,Downstream 12,Downstream 13,Downstream 14,Downstream 15,Downstream 16" > $DLOGSNR
+echo "$RUNDATE,$DS1,$DS2,$DS3,$DS4,$DS5,$DS6,$DS7,$DS8,$DS9,$DS10,$DS11,$DS12,$DS13,$DS14,$DS15,$DS16" >> $DLOGSNR
 
 # upstream freq log
-[[ ! -f $ULOGFREQ ]] && echo "DTS,Upstream 1,Upstream 2,Upstream 3" > $ULOGFREQ
-echo "$RUNDATE,${UF1/ *},${UF2/ *},${UF3/ *}" >> $ULOGFREQ
+[[ ! -f $ULOGFREQ ]] && echo "DTS,Upstream 1,Upstream 2,Upstream 3,Upstream 4" > $ULOGFREQ
+echo "$RUNDATE,${UF1/ *},${UF2/ *},${UF3/ *},${UF4/ *}" >> $ULOGFREQ
 
 # upstream power log
-[[ ! -f $ULOGPOWER ]] && echo "DTS,Upstream 1,Upstream 2,Upstream 3" > $ULOGPOWER
-echo "$RUNDATE,$UP1,$UP2,$UP3" >> $ULOGPOWER
+[[ ! -f $ULOGPOWER ]] && echo "DTS,Upstream 1,Upstream 2,Upstream 3,Upstream 4" > $ULOGPOWER
+echo "$RUNDATE,$UP1,$UP2,$UP3,$UP4" >> $ULOGPOWER
